7a9dc5753a23ebc3dc249664a22ec6266feae581
kaivladimirv <kaivladimirv@gmail.com>
2023-02-14 10:28:16 +0600

    feature: Add Home page

--- public/index.php
+++ public/index.php
@@ -2,18 +2,28 @@
 
 declare(strict_types=1);
 
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+AppFactory::setContainer($container);
+
+$container->set('view', function () {
+    return Twig::create('../templates');
+});
+
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write('Welcome!');
+$app->add(TwigMiddleware::createFromContainer($app));
 
-    return $response;
-});
+$app->get('/', function (Request $request, Response $response) {
+    return $this->get('view')->render($response, 'app/home.html.twig');
+})->setName('home');
 
 $app->run();
